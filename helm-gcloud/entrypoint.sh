f14056103536d9f7033a1e2f767aafb445e1cfc3
Per Lundh <per.lundh@careofcrm.com>
2020-12-15 20:12:34 +0100

    Fix shell quoting

--- helm-gcloud/entrypoint.sh
+++ helm-gcloud/entrypoint.sh
@@ -3,12 +3,12 @@
 set -e
 
 echo "$INPUT_GCLOUDAUTH" | base64 -d > "$HOME"/gcloud.json
-gcloud auth activate-service-account --key-file=$HOME/gcloud.json $*
-gcloud config set project ${INPUT_PROJECTNAME}
-gcloud container clusters get-credentials ${INPUT_CLUSTERNAME} --zone ${INPUT_CLUSTERZONE}
+gcloud auth activate-service-account --key-file="$HOME"/gcloud.json $*
+gcloud config set project "$INPUT_PROJECTNAME"
+gcloud container clusters get-credentials "$INPUT_CLUSTERNAME" --zone "$INPUT_CLUSTERZONE"
 
 if [ -f "$INPUT_VALUES" ]; then
-	helm upgrade --install ${INPUT_RELEASENAME} --values ${INPUT_VALUES} --set x=x,${INPUT_SET} ${INPUT_CHARTPATH}
+	helm upgrade --install "$INPUT_RELEASENAME" --values "$INPUT_VALUES" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
 else
-	helm upgrade --install ${INPUT_RELEASENAME} --set x=x,${INPUT_SET} ${INPUT_CHARTPATH}
+	helm upgrade --install "$INPUT_RELEASENAME" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
 fi
