1aaafa5ed1dde48dfef831aff5d550fb792016ba
Per Lundh <per.lundh@careofcrm.com>
2020-12-15 20:22:07 +0100

    Add namespace input

--- helm-gcloud/entrypoint.sh
+++ helm-gcloud/entrypoint.sh
@@ -8,7 +8,7 @@ gcloud config set project "$INPUT_PROJECTNAME"
 gcloud container clusters get-credentials "$INPUT_CLUSTERNAME" --zone "$INPUT_CLUSTERZONE"
 
 if [ -f "$INPUT_VALUES" ]; then
-	helm upgrade --install "$INPUT_RELEASENAME" --values "$INPUT_VALUES" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
+	helm upgrade --namespace "$INPUT_NAMESPACE" --install "$INPUT_RELEASENAME" --values "$INPUT_VALUES" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
 else
-	helm upgrade --install "$INPUT_RELEASENAME" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
+	helm upgrade --namespace "$INPUT_NAMESPACE" --install "$INPUT_RELEASENAME" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
 fi
