aaa34f8d481e5256a1df01c0338b5881c7de823a
Per Lundh <per.lundh@careofcrm.com>
2020-12-15 18:03:59 +0100

    Add parameter for helm values file

--- helm-gcloud/entrypoint.sh
+++ helm-gcloud/entrypoint.sh
@@ -7,4 +7,8 @@ gcloud auth activate-service-account --key-file=$HOME/gcloud.json $*
 gcloud config set project ${INPUT_PROJECTNAME}
 gcloud container clusters get-credentials ${INPUT_CLUSTERNAME} --zone ${INPUT_CLUSTERZONE}
 
-helm upgrade ${INPUT_RELEASENAME} --set x=x,${INPUT_SET} ${INPUT_CHARTPATH}
+if [ -f $INPUT_VALUES ]; then
+	helm upgrade ${INPUT_RELEASENAME} --values ${INPUT_VALUES} --set x=x,${INPUT_SET} ${INPUT_CHARTPATH}
+else
+	helm upgrade ${INPUT_RELEASENAME} --set x=x,${INPUT_SET} ${INPUT_CHARTPATH}
+fi
