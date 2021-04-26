26babbd6e535aa82559e35ea4ca0122c25275be5
Per Lundh <per.lundh@careofcrm.com>
2021-04-26 13:43:58 +0200

    Support setting --atomic

--- helm-gcloud/entrypoint.sh
+++ helm-gcloud/entrypoint.sh
@@ -7,8 +7,14 @@ gcloud auth activate-service-account --key-file="$HOME"/gcloud.json $*
 gcloud config set project "$INPUT_PROJECTNAME"
 gcloud container clusters get-credentials "$INPUT_CLUSTERNAME" --zone "$INPUT_CLUSTERZONE"
 
+helm_args=
+
+if [ "$INPUT_ATOMIC" = "true" ]; then
+	helm_args=$helm_args --atomic
+fi
+
 if [ -f "$INPUT_VALUES" ]; then
-	helm upgrade --namespace "$INPUT_NAMESPACE" --install "$INPUT_RELEASENAME" --values "$INPUT_VALUES" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
+	helm upgrade $helm_args --namespace "$INPUT_NAMESPACE" --install "$INPUT_RELEASENAME" --values "$INPUT_VALUES" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
 else
-	helm upgrade --namespace "$INPUT_NAMESPACE" --install "$INPUT_RELEASENAME" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
+	helm upgrade $helm_args --namespace "$INPUT_NAMESPACE" --install "$INPUT_RELEASENAME" --set x=x,"$INPUT_SET" "$INPUT_CHARTPATH"
 fi
